ec1c0dca2927713fc8a647de3d5a152ac8a8c246
Sanowar72 <atifreza72ar@gmail.com>
2024-11-03 01:05:47 +0530

    typo mistake fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className="bg-emerald-100 py-24 pt-40" id="home">
       <div className="align-element grid md:grid-cols-2 items-center gap-8">
         <article>
-          <h1 className="text-7xl font-bold tracking-wider">I'am Sanowar</h1>
+          <h1 className="text-7xl font-bold tracking-wider">I'm Sanowar</h1>
           <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
             Full-Stack Developer
           </p>
